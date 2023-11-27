524b1ffda761c4ed2c1a37ac469b6833dbb5c6c4
Mathias Collas-Jourdan <mathias.collas@gmail.com>
2023-11-27 18:50:15 +0100

    Add comment & run loop

--- client.py
+++ client.py
@@ -1,24 +1,31 @@
 import socket
 import threading
+import keyboard
 import time
+
 MSGLEN = 256
+run = True
 
 class MySocket:
-
     def __init__(self, sock=None):
+        # Constructor to initialize the socket object.
         if sock is None:
-            self.sock = socket.socket(
-                            socket.AF_INET, socket.SOCK_STREAM)
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         else:
             self.sock = sock
 
     def connect(self, host, port):
+        # Connect to the specified host and port.
         self.sock.connect((host, port))
-    
+
     def deconnect(self):
+        # Close the socket connection.
+        global run
+        run = False
         self.sock.close()
 
     def mysend(self, msg):
+        # Send a message over the socket.
         totalsent = 0
         while totalsent < MSGLEN:
             try:
@@ -27,11 +34,22 @@ class MySocket:
                     break
                 totalsent += sent
             except Exception as e:
+                # Handle any exceptions that may occur during sending.
                 print(f"Error in mysend: {e}")
-                break  # Sort de la boucle si une exception est levée
+                break
 
-    
+    def write_message(self):
+        try:
+            # Prompt user for a message and send it when the space key is pressed.
+            message = input("Enter the message: ")
+            if len(message)>0 and message != ' ':
+                self.mysend(message)
+        except Exception as e:
+            # Handle any exceptions that may occur during message input or sending.
+            print(f"Error: {e}")
+
     def myreceive(self):
+        # Receive a message from the socket.
         chunks = []
         bytes_recd = 0
         while bytes_recd < MSGLEN:
@@ -48,15 +66,11 @@ client = MySocket()
 # listening_port = int(input("Enter listening port : "))
 # client.connect(network_ip, listening_port)
 client.connect('127.0.0.1', 5000)
-while True:
-    try:
-        message = input("Entrer le message : ")
-        if message == "!EXIT":
-            break
-        else:
-            client.mysend(message)
-    except Exception as e:
-        print(f"Error: {e}")
-        break
 
-client.deconnect()
+while run:
+    if keyboard.is_pressed('ctrl'):
+        client.write_message()
+    elif keyboard.is_pressed('esc'):
+        client.deconnect()
+    else:
+        time.sleep(0.5)
