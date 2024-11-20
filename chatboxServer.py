4eadebaba44f8cf884bdc5243837536840a25da1
M4THIA5 <mathias.collas@gmail.com>
2024-11-20 23:43:35 +0100

    feat: chatbox whith many clients

--- chatboxServer.py
+++ chatboxServer.py
@@ -15,6 +15,13 @@ async def disconnect(sid):
     print(f'Client déconnecté : {sid}')
     connected_clients.remove(sid)
 
+@sio.event
+async def message(sid, data):
+    print(f"Message reçu de {sid} : {data}")
+    for client in connected_clients:
+        if client != sid:
+            await sio.emit('message', f"{sid} -> {data}", to=client)
+
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run(app, host='0.0.0.0', port=4000)
